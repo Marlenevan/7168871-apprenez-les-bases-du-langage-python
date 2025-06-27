1fd6e0347b20399d40f0df42bccbf5936be89431
Marlenevan <marlenekouayep@gmail.com>
2025-06-27 00:42:08 -0400

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,11 @@
 # Écrivez votre code ici !
+nom="Marléne"
+age=31
+taille=1.64
+Est_Etudiant=false
+print(f"je suis {nom}, j'ai {age} ans,je mesure {taille} ")
+print(Est_Etudiant)
+type(nom)
+type(age)
+type(taille)
+type(Est_Etudiant)
