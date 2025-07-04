c136baf8b1c83c3dbaba2cd53d57224ac794eed4
Marlenevan <marlenekouayep@gmail.com>
2025-07-03 20:54:52 -0400

    Update main.py

--- P2/P2C2/énoncé/main.py
+++ P2/P2C2/énoncé/main.py
@@ -1,1 +1,18 @@
 # Ecrivez votre code ici !
+nombres=input("entrer une liste de nombres separée par des virgules: ")
+liste=nombres.split(",")
+for i in range(len(liste)):
+	liste[i]=int(liste[i])
+j=0
+somme=0
+while j<len(liste):
+     somme=somme + liste[j]
+     j+=1
+moyenne=somme/len(liste)
+print(f"la somme est: {somme}")
+print(f"la moyenne est: {moyenne}")
+nbre=0
+for i in range(len(liste)):
+    if liste[i]>moyenne:
+        nbre+=1
+print(f"{nbre} nombres sont superieurs à la moyenne")
